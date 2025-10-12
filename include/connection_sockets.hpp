3b6b7c85ef925839ceefdfb58cfbcf2ed6291f90
Les-S-niK <lessnik343@gmail.com>
2025-10-12 12:46:53 +0300

    feat: Add basic TCP socket implementation.
    - Added connection_sockets.hpp/cpp files
    - Added new class Sockets with bind, listen and accept methods.
    - Added first CMakeLists.txt.
    - Added TODO markers in the code for future enhancements.

---
+++ include/connection_sockets.hpp
@@ -0,0 +1,32 @@
+
+#include <cstdint>
+#include <string>
+#include <functional>
+
+
+// Max backlog size for listen function. 10 is for tests. 128 or 4096 for production.
+static uint8_t max_backlog_size = 10;
+
+
+class Sockets
+{
+    // TODO: Implement IPv6 support in future.
+    public:
+        int fd;
+
+        Sockets(
+            uint16_t port,
+            std::string ipv4_host = "0.0.0.0"
+        );
+
+        void acceptConnection();
+
+    
+    private:
+        int client_fd;
+        uint16_t port;
+        std::string host;
+
+        inline void bindSocket();
+        inline void initSocket();
+};
---
+++ src/connection_sockets.cpp
@@ -0,0 +1,62 @@
+
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "../include/connection_sockets.hpp"
+
+
+// TODO: Implement IPv6 support in future.
+Sockets::Sockets(
+    uint16_t port,
+    std::string ipv4_host
+) : 
+    port(port),
+    host(ipv4_host)
+{
+    initSocket();
+    bindSocket();
+}
+
+void Sockets::acceptConnection() {
+    struct sockaddr_in client_addr_in;
+    socklen_t client_socklen = sizeof(client_addr_in);
+    listen(fd, max_backlog_size);
+    // TODO: Make reasonable condition in future.
+    while(true) {
+        client_fd = accept(fd, (struct sockaddr *) &client_addr_in, &client_socklen);
+        if(client_fd == -1) {
+            close(client_fd);
+            // TODO: Make exception class and throw it.
+            throw "Can't get client fd";
+        }
+        // TODO: Add recv, and send function calls. 
+
+        close(client_fd);
+    }
+}
+
+inline void Sockets::bindSocket() {
+    struct sockaddr_in addr_in;
+
+    addr_in.sin_port = htons(port);
+    addr_in.sin_family = AF_INET; 
+    inet_aton(host.c_str(), &addr_in.sin_addr);
+
+    if(bind(fd, (const struct sockaddr *) &addr_in, sizeof(addr_in)) == -1) {
+        close(fd);
+        // TODO: Make exception class and throw it.
+        throw "Can't bind socket.";
+    }
+}
+
+inline void Sockets::initSocket() {
+    fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(fd == -1) {
+        close(fd);
+        // TODO: Make exception class and throw it.
+        throw "Can't create TCP socket.";
+    } 
+}
+
