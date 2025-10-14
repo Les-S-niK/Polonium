9a6eb08b30c77976815c91c8f6bac3bfea1595e0
Les-S-niK <lessnik343@gmail.com>
2025-10-14 22:28:30 +0300

    feat: Implemented first version of headers parsing logic using llhttp.
    - Added callback-handlers to get http method, url, version, protocol, headers and body.
    - Added first version of documentation for callback-handlers in http_parser.hpp
    - Added TODOs to improve the code in future.
    
    The current version of the wrapper should parse http packets correctly.

--- include/http_parser.hpp
+++ include/http_parser.hpp
@@ -1,5 +1,7 @@
 #pragma once
+
 #include <string>
+#include <unordered_map>
 
 #include "../third_party/llhttp/llhttp.h"
 
@@ -7,16 +9,113 @@
 class HttpRequestsParser
 {
     public:
-        HttpRequestsParser(const std::string& request);
+        std::string url;
+        std::string method;
+        std::string protocol;
+        std::string version;
+        std::unordered_map<std::string, std::string> headers;
+        std::string body;
 
+        HttpRequestsParser(const std::string& request);
+        
     private:
         llhttp_t parser;
         llhttp_settings_t settings;
         const std::string request;
-        
+        /**
+         * @brief Temporary pair presents <field: value> in the request headers.
+         * The first element sets in the <handler_on_header_field>
+         * The second element sets in the <handler_on_header_value>
+         * The pair will be added in the <headers> in the <handler_on_header_value_complete>.
+         */
+        std::pair<std::string, std::string> temporary_pair;
+
         void setCallbacks();
         void parse();
 
         // Callback llhttp methods.
+
+        /**
+         * @brief Handler start when http packet begins.
+         * 
+         * @param parser 
+         * @return int 
+         */
         static int handler_on_message_begin(llhttp_t* parser);
+
+        /**
+         * @brief Handler start when llhttp finds http body.
+         * 
+         * @param parser 
+         * @return int 
+         */
+        static int handler_on_body(llhttp_t* parser, const char* at, size_t length);
+        
+        /**
+        * @brief Handler starts when llhttp finds url.
+        * 
+        * @param parser 
+        * @param at
+        * @param length 
+        * @return int - erorr code.
+        */
+        static int handler_on_url(llhttp_t* parser, const char* at, size_t length);
+
+        /**
+        * @brief Handler starts when llhttp finds http method [GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, TRACE, CONNECT]
+        * 
+        * @param parser 
+        * @param at 
+        * @param length 
+        * @return int 
+        */
+        static int handler_on_method(llhttp_t* parser, const char* at, size_t length);
+
+        /**
+        * @brief Handler starts when llhttp finds protocol type [HTTP for example]
+        * 
+        * @param parser 
+        * @param at 
+        * @param length 
+        * @return int 
+        */
+        static int handler_on_protocol(llhttp_t* parser, const char* at, size_t length);
+
+        /**
+        * @brief Handler starts when llhttp finds protocol version. [1.1; 2; etc... for example]
+        * 
+        * @param parser 
+        * @param at 
+        * @param length 
+        * @return int 
+        */
+        static int handler_on_version(llhttp_t* parser, const char* at, size_t length);
+
+        /**
+        * @brief Handler starts when llhttp finds header field [<field>: value];
+        * 
+        * @param parser 
+        * @param at 
+        * @param length 
+        * @return int 
+        */
+        static int handler_on_header_field(llhttp_t* parser, const char* at, size_t length);
+
+        /**
+        * @brief Handler starts when llhttp finds header value [field: <value>];
+        * 
+        * @param parser 
+        * @param at 
+        * @param length 
+        * @return int 
+        */
+        static int handler_on_header_value(llhttp_t* parser, const char* at, size_t length);
+
+        /**
+         * @brief Handler starts when llhttp ends the headers parsing.
+         * 
+         * @param parser 
+         * @return int 
+         */
+        static int handler_on_header_value_complete(llhttp_t* parser);
 };
--- src/http_parser.cpp
+++ src/http_parser.cpp
@@ -6,25 +6,87 @@
 
 HttpRequestsParser::HttpRequestsParser(const std::string& request) : request(request) {
     llhttp_settings_init(&settings);
-    setCallbacks();
     llhttp_init(&parser, HTTP_REQUEST, &settings);
+    // Set <parser.data> to manage current class instance in the handlers.
+    parser.data = this;
+
+    setCallbacks();
     parse();
 }
 
 void HttpRequestsParser::setCallbacks() {
     settings.on_message_begin = handler_on_message_begin;
+    settings.on_url = handler_on_url;
+    settings.on_method = handler_on_method;
+    settings.on_protocol = handler_on_protocol;
+    settings.on_version = handler_on_version;
+    settings.on_header_field = handler_on_header_field;
+    settings.on_header_value = handler_on_header_value;
+    settings.on_header_value_complete = handler_on_header_value_complete;
+    settings.on_body = handler_on_body;
 }
 
 void HttpRequestsParser::parse() {
-    enum llhttp_errno error_num = llhttp_execute(&parser, request.c_str(), request.length());    
+    enum llhttp_errno error_num = llhttp_execute(
+        &parser, request.c_str(), request.length());    
     if(error_num != 0) {
         // TODO: Throw an exception class here in future.
+        std::cerr << "Error code: " << error_num << std::endl;
         throw "The error code is bad.";
     }
 }
 
+
 // Callback methods realization.
 int HttpRequestsParser::handler_on_message_begin(llhttp_t* parser) {
-    std::cout << "Begin" << std::endl;
+    std::cout << "Begin..." << std::endl;
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_url(llhttp_t* parser, const char* at, size_t length) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->url = std::string(at, length);
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_method(llhttp_t* parser, const char* at, size_t length) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->method = std::string(at, length);
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_protocol(llhttp_t* parser, const char* at, size_t length) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->protocol = std::string(at, length);
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_version(llhttp_t* parser, const char* at, size_t length) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->version = std::string(at, length);
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_header_field(llhttp_t* parser, const char* at, size_t length) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->temporary_pair.first = std::string(at, length); 
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_header_value(llhttp_t* parser, const char* at, size_t length) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->temporary_pair.second = std::string(at, length);  
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_header_value_complete(llhttp_t* parser) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->headers.emplace(self->temporary_pair);  
+    return 0;
+}
+
+int HttpRequestsParser::handler_on_body(llhttp_t* parser, const char* at, size_t length) {
+    HttpRequestsParser* self = static_cast<HttpRequestsParser*>(parser->data);
+    self->body = std::string(at, length);
     return 0;
 }
