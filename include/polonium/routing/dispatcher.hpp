25583980a781464ee3f3906b0165f6bf09a800e1
Les-S-niK <lessnik343@gmail.com>
2026-03-27 21:50:52 +0300

    refactor(core): Refactor routing part of the framework.

--- include/polonium/routing/dispatcher.hpp
+++ include/polonium/routing/dispatcher.hpp
@@ -6,40 +6,47 @@
 #include <optional>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 #include "polonium/api_responses.hpp"
 #include "polonium/http/http.hpp"
 #include "polonium/polonium_logger.hpp"
 #include "polonium/routing/uri_parser.hpp"
 
+// TODO: Replace std::function by template with concept.
+using endpoint_handler =
+    std::function<std::shared_ptr<ApiResponse>(HttpRequest&& request)>;
+using routes_table = std::unordered_map<
+    std::string, std::unordered_map<std::string, std::pair<endpoint_handler,
+                                                           parsed_templates>>>;
 
-using endpoint_handler = std::function<std::shared_ptr<ApiResponse> (HttpRequest&& request)>;
-using routes_table = std::unordered_map<std::string, std::unordered_map<std::string, std::pair<endpoint_handler, parsed_templates>>>;
-
-
-struct HandlerWithParams
-{
-    HandlerWithParams(endpoint_handler handler, std::unordered_map<std::string, UriParamValue> path_params)
-     :
-       handler(handler), path_params(path_params)
-    {}
-    HandlerWithParams(std::unordered_map<std::string, UriParamValue> path_params) : path_params(path_params) {}
-    HandlerWithParams() {}
+struct HandlerWithParams {
+    HandlerWithParams(
+        endpoint_handler handler,
+        std::unordered_map<std::string, UriParamValue> path_params)
+        : handler(handler), path_params(std::move(path_params)) {}
+    HandlerWithParams(
+        std::unordered_map<std::string, UriParamValue> path_params)
+        : path_params(std::move(path_params)) {}
+    HandlerWithParams() = default;
 
     std::optional<endpoint_handler> handler = std::nullopt;
     std::unordered_map<std::string, UriParamValue> path_params;
 };
 
+class Dispatcher {
+   public:
+    Dispatcher(PoloniumLogger& logger) : logger_(logger) {
+        logger.trace(__func__);
+    }
+    ~Dispatcher() { logger_.trace(__func__); }
+    void registerMethod(std::string&& method, std::string&& uri,
+                        endpoint_handler&& handler,
+                        parsed_templates&& templates);
+    auto checkRoute(const std::string& method, const std::string& uri) const
+        -> HandlerWithParams;
 
-class Dispatcher
-{
-    public:
-        Dispatcher(PoloniumLogger& logger) : logger_(logger) { logger.trace(__func__); }
-        ~Dispatcher() { logger_.trace(__func__); }
-        void registerMethod(std::string&& method, std::string&& uri, endpoint_handler&& handler, parsed_templates&& templates);
-        HandlerWithParams checkRoute(const std::string& method, const std::string& uri) const;
-    
-    private:
-        PoloniumLogger& logger_;
-        routes_table routes_; 
+   private:
+    PoloniumLogger& logger_;
+    routes_table routes_;
 };
--- include/polonium/routing/uri_parser.hpp
+++ include/polonium/routing/uri_parser.hpp
@@ -2,85 +2,77 @@
 #pragma once
 
 #include <optional>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <sstream>
 
 /*
 Since I won't change ctre to another way to use regex in compile-time,
 I don't want to make a header with interfaces for this third-party lib.
-*/ 
+*/
 #include <ctre.hpp>
-#include "polonium/routing/uri_params.hpp"
 
+#include "polonium/routing/uri_params.hpp"
 
 using parsed_templates = std::unordered_map<unsigned, UriParamTemplate>;
 
+inline auto splitUri(std::string_view uri, char separator = '/')
+    -> std::vector<std::string>;
 
-inline std::vector<std::string> splitUri(std::string_view uri, const char separator = '/') {
-    std::vector<std::string> splitted_uri;
-    splitted_uri.reserve(5);
-    std::string token, uri_str(uri);
-    std::istringstream token_stream(uri_str);
-    
-    while(std::getline(token_stream, token, separator)) {
-        if(!token.empty()) 
-            splitted_uri.push_back(token);
-    }
-    return splitted_uri;
-}
+class UriParser {
+   public:
+    UriParser(std::string_view uri, std::string_view uri_template)
+        : uri_(uri), uri_template_(uri_template) {}
+    UriParser(const UriParser&) noexcept = default;
+    UriParser(const UriParser&&) noexcept = delete;
+    auto operator=(const UriParser&) noexcept -> UriParser& = default;
+    auto operator=(const UriParser&&) noexcept -> UriParser& = delete;
 
+    [[nodiscard]] auto getUriParamsByTemplate(parsed_templates params_template)
+        const -> std::unordered_map<std::string, UriParamValue>;
 
-class UriParser
-{
-    public:
-        UriParser(std::string_view uri, std::string_view uri_template)
-         :
-          uri_(uri),
-          uri_template_(uri_template)
-        {}
-        UriParser(const UriParser&) noexcept = default;
-        UriParser(const UriParser&&) noexcept = delete;
-        UriParser& operator=(const UriParser&) noexcept = default;
-        UriParser& operator=(const UriParser&&) noexcept = delete;
-        
-        std::unordered_map<std::string, UriParamValue> 
-        getUriParamsByTemplate(parsed_templates params_template) const;
-    
-    private:
-        std::string_view uri_;
-        std::string_view uri_template_;
+   private:
+    std::string_view uri_;
+    std::string_view uri_template_;
 
-        bool tryConvertToInt(const std::string& value) const {
-            size_t pos{}; 
-            
-            try { std::stoi(value, &pos); } 
-            catch(const std::invalid_argument&) { return false; }
-            catch(const std::out_of_range&) { return false; }
-            
-            if(value.size() != pos) return false;
-            return true;
+    [[nodiscard]]
+    static auto tryConvertToInt(const std::string& value) -> bool {
+        size_t pos{};
+
+        try {
+            std::stoi(value, &pos);
+        } catch (const std::invalid_argument&) {
+            return false;
+        } catch (const std::out_of_range&) {
+            return false;
         }
+
+        return value.size() == pos;
+    }
 };
 
+class UriTemplateParser {
+   public:
+    UriTemplateParser(std::string_view uri_template)
+        : uri_template_(uri_template) {}
+    UriTemplateParser(const UriTemplateParser&) noexcept = default;
+    UriTemplateParser(const UriTemplateParser&&) noexcept = delete;
+    auto operator=(const UriTemplateParser&) noexcept
+        -> UriTemplateParser& = default;
+    auto operator=(const UriTemplateParser&&) noexcept
+        -> UriTemplateParser& = delete;
 
-class UriTemplateParser
-{
-    public:
-        UriTemplateParser(std::string_view uri_template) : uri_template_(uri_template) {}
-        UriTemplateParser(const UriTemplateParser&) noexcept = default;
-        UriTemplateParser(const UriTemplateParser&&) noexcept = delete;
-        UriTemplateParser& operator=(const UriTemplateParser&) noexcept = default;
-        UriTemplateParser& operator=(const UriTemplateParser&&) noexcept = delete;
-        
-        parsed_templates getUriParamsTemplate() const;
+    [[nodiscard]] auto getUriParamsTemplate() const -> parsed_templates;
 
-    private:
-        static constexpr auto pattern = ctll::fixed_string{"(?<type>\\w+)\\s+(?<name>\\w+)"};
-        std::string_view uri_template_;
+   private:
+    static constexpr auto pattern =
+        ctll::fixed_string{R"((?<type>\w+)\s+(?<name>\w+))"};
+    std::string_view uri_template_;
 
-        bool validateUriTemplateFirst() const noexcept;
-        std::optional<std::pair<std::string, std::string>> validateUriTemplateSecond(std::string_view uri_param_template) const;
+    [[nodiscard]] auto validateUriTemplateFirst() const noexcept -> bool;
+    [[nodiscard]] static auto validateUriTemplateSecond(
+        std::string_view uri_param_template)
+        -> std::optional<std::pair<std::string, std::string>>;
 };
