1b6570969c8ad531007b3620dcbc18468d0bb045
Les-S-niK <lessnik343@gmail.com>
2026-05-04 22:02:32 +0300

    perf(core): Replace std::exception throwing by std::expected.
    - Remove socket_exceptions header.
    - Change tcp_socket methods return types.

--- src/sockets/tcp_socket.cpp
+++ src/sockets/tcp_socket.cpp
@@ -1,19 +1,19 @@
 #include "polonium/sockets/tcp_socket.hpp"
 
+#include <arpa/inet.h>
 #include <asm-generic/socket.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
 #include <array>
 #include <cerrno>
+#include <expected>
 #include <format>
 #include <iterator>
+#include <stdexcept>
 #include <system_error>
 
-#include "polonium/sockets/socket_exceptions.hpp"
-
-// FIXME: Replace logAndThrowException_ method with "throw" by std::expected
-// with enum statuses. Delete STUB exceptions.
+#include "polonium/polonium_logger.hpp"
 
 TcpIpv4Socket::TcpIpv4Socket()
     : logger_(PoloniumLogger::getInstance()),
@@ -22,7 +22,9 @@ TcpIpv4Socket::TcpIpv4Socket()
     logger_->info("TCP/IPv4 socket initialization.");
 
     if (server_fd_ == -1) {
-        logAndThrowException_(exception_messages::tcp_socket);
+        logSocketError_(exception_messages::tcp_socket,
+                        &PoloniumLogger::critical);
+        throw std::runtime_error(exception_messages::tcp_socket);
     }
     logger_->debug("Created TCP/IPv4 socket.");
 }
@@ -36,14 +38,15 @@ TcpIpv4Socket::~TcpIpv4Socket() {
     logger_->debug("Closed TCP/IPv4 socket.");
 }
 
-[[nodiscard]] auto TcpIpv4Socket::getServerSocketFd() const -> socket_fd {
+auto TcpIpv4Socket::getServerSocketFd() const -> socket_fd {
     logger_->trace(__func__);
     return server_fd_;
 }
 
-auto TcpIpv4Socket::tcpBind(const std::string& host, const uint16_t& port)
-    -> void {
+auto TcpIpv4Socket::tcpBind(const std::string& host, const uint16_t& port) const
+    -> std::expected<void, TcpSocketErrors> {
     logger_->trace(__func__);
+
     struct sockaddr_in server_addr{};
     inet_aton(host.c_str(), &server_addr.sin_addr);
     server_addr.sin_port = htons(port);
@@ -56,27 +59,36 @@ auto TcpIpv4Socket::tcpBind(const std::string& host, const uint16_t& port)
             0 or
         setsockopt(server_fd_, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) <
             0) {
-        logAndThrowException_(
-            "Can not set socket option to reuse ADDR or PORT.");
+        logSocketError_(exception_messages::tcp_bind_sockopt_error,
+                        &PoloniumLogger::warning);
     }
 
     if (bind(server_fd_, reinterpret_cast<struct sockaddr*>(&server_addr),
              sizeof(server_addr)) == -1) {
-        logAndThrowException_(exception_messages::tcp_bind);
+        logSocketError_(exception_messages::tcp_bind,
+                        &PoloniumLogger::critical);
+        return std::unexpected(TcpSocketErrors::TcpSocketBindError);
     }
     logger_->debug("TCP/IPv4 socket binded.");
+    return {};
 }
 
-auto TcpIpv4Socket::tcpListen(const int& max_backlog_size) -> void {
+auto TcpIpv4Socket::tcpListen(const int& max_backlog_size) const
+    -> std::expected<void, TcpSocketErrors> {
     logger_->trace(__func__);
     if (listen(server_fd_, max_backlog_size) == -1) {
-        logAndThrowException_(exception_messages::tcp_listen);
+        logSocketError_(exception_messages::tcp_listen,
+                        &PoloniumLogger::critical);
+        return std::unexpected(TcpSocketErrors::TcpSocketListenErorr);
     }
     logger_->debug(std::format("Listen. Max backlog size: {} connections",
                                max_backlog_size));
+    return {};
 }
 
-auto TcpIpv4Socket::tcpAccept() -> std::pair<socket_fd, struct sockaddr_in> {
+auto TcpIpv4Socket::tcpAccept() const
+    -> std::expected<std::pair<socket_fd, struct sockaddr_in>,
+                     TcpSocketErrors> {
     logger_->trace(__func__);
     struct sockaddr_in client_addr{};
     socklen_t client_addr_len = sizeof(client_addr);
@@ -88,13 +100,13 @@ auto TcpIpv4Socket::tcpAccept() -> std::pair<socket_fd, struct sockaddr_in> {
         if (int errno_core =
                 std::error_code(errno, std::generic_category()).value();
             errno_core == EINTR) {
-            logger_->warning(
-                "Got SIGINT and correctly process this signal. Do not look at "
-                "exception below.");
-            // FIXME:
-            logAndThrowException_("STUB exception. Change after refactoring.");
+            logSocketError_(exception_messages::tcp_accept_interrupted,
+                            &PoloniumLogger::warning);
+            return std::unexpected(
+                TcpSocketErrors::TcpSocketAcceptInterruptError);
         }
-        logAndThrowException_(exception_messages::tcp_accept);
+        logSocketError_(exception_messages::tcp_accept, &PoloniumLogger::error);
+        return std::unexpected(TcpSocketErrors::TcpSocketAcceptInvalidFdError);
     }
     std::array<char, praddr4_len> protoaddr{};
     inet_ntop(AF_INET, &client_addr.sin_addr, protoaddr.data(),
@@ -107,8 +119,8 @@ auto TcpIpv4Socket::tcpAccept() -> std::pair<socket_fd, struct sockaddr_in> {
     return std::make_pair(client_fd, client_addr);
 }
 
-auto TcpIpv4Socket::tcpRecv(const socket_fd& client_fd, const int flags)
-    -> std::string {
+auto TcpIpv4Socket::tcpRecv(const socket_fd& client_fd, const int flags) const
+    -> std::expected<std::string, TcpSocketErrors> {
     logger_->trace(__func__);
     std::array<char, socket_options::max_buffer_size> buffer{};
 
@@ -119,35 +131,32 @@ auto TcpIpv4Socket::tcpRecv(const socket_fd& client_fd, const int flags)
         if (int errno_code =
                 std::error_code(errno, std::generic_category()).value();
             errno_code == EAGAIN or errno_code == EWOULDBLOCK) {
-            logger_->warning(
-                "Correctly proccessed TIMEOUT exception. Do not look at "
-                "exception below.");
-            // FIXME:
-            logAndThrowException_("STUB exception. Change after refactoring.");
+            logSocketError_(exception_messages::tcp_recv_timeout,
+                            &PoloniumLogger::warning);
+            return std::unexpected(TcpSocketErrors::TcpSocketRecvTimeoutError);
         }
-        logAndThrowException_(exception_messages::tcp_recv);
-    } else if (recieved_size == 0) {
-        return {};
+        logSocketError_(exception_messages::tcp_recv, &PoloniumLogger::error);
+        return std::unexpected(TcpSocketErrors::TcpSocketRecvInvalidSizeError);
+    }
+    if (recieved_size == 0) {
+        return std::string{};
     }
     logger_->debug(
         std::format("Recieved buffer with the size: {}", recieved_size));
-    return {buffer.begin(), std::next(buffer.begin(), recieved_size)};
+    return std::string{buffer.begin(),
+                       std::next(buffer.begin(), recieved_size)};
 }
 
 auto TcpIpv4Socket::tcpSend(const socket_fd& client_fd, std::string_view buffer,
-                            const int flags) -> void {
+                            const int flags) const
+    -> std::expected<ssize_t, TcpSocketErrors> {
     logger_->trace(__func__);
-    if (send(client_fd, buffer.data(), buffer.size(), flags) == -1) {
-        logAndThrowException_(exception_messages::tcp_send);
+    ssize_t sent_size = send(client_fd, buffer.data(), buffer.size(), flags);
+
+    if (sent_size == -1) {
+        logSocketError_(exception_messages::tcp_send, &PoloniumLogger::error);
+        return std::unexpected(TcpSocketErrors::TcpSocketSendError);
     }
     logger_->debug(std::format("Sent buffer with the size: {}", buffer.size()));
-}
-
-auto TcpIpv4Socket::logAndThrowException_(std::string_view message) const
-    -> void {
-    std::string final_message = std::format(
-        "{}. Errno: {}", message,
-        (std::error_code(errno, std::generic_category()).message()));
-    logger_->critical(final_message);
-    throw socket_exception(final_message);
+    return sent_size;
 }
